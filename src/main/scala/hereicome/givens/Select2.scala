48968e01d9aa1f74b8cb3ef5f2a09a12f2684ac5
Walter Chang <weihsiu@gmail.com>
2019-08-30 01:49:51 +0800

    select done

--- src/main/scala/hereicome/givens/Select2.scala
+++ src/main/scala/hereicome/givens/Select2.scala
@@ -1,35 +1,45 @@
 package hereicome.givens
 
-opaque type RandomInt = Int
+opaque type RandomInt = (Int, Int)
 object RandomInt {
-  def apply()
+  def apply(start: Int, end: Int): RandomInt = (start, end)
+  def range(randomInt: RandomInt): (Int, Int) = randomInt
 }
 
-trait Selector[A] {
-  def value(x: A): A
+trait Selector[A, B] {
+  def value(x: A): B
 }
 
 object Selector {
-  given [A] as Selector[A] {
+  given [A] as Selector[A, A] {
     def value(x: A) = x
+  }
+
+  given as Selector[RandomInt, Int] {
+    def value(x: RandomInt) = {
+      val (start, end) = RandomInt.range(x)
+      scala.util.Random.nextInt(end - start) + start
+    }
   }
 }
 
-trait Select2[A, B] {
+trait Select2[A, B, C] {
   type Out
-  def (x: A) select (selector: B) given Selector[B]: Option[Out]
+  def (x: A) select (selector: B) given Selector[B, C]: Option[Out]
 }
 
 object Select2 {
-  given [A] as Select2[IndexedSeq[A], Int] {
+  given [A, B] as Select2[IndexedSeq[A], B, Int] {
     type Out = A
-    def (x: IndexedSeq[A]) select (selector: Int) given (S: Selector[Int]): Option[A] =
-      if (x.length > selector) Some(x(S.value(selector))) else None
+    def (x: IndexedSeq[A]) select (selector: B) given (S: Selector[B, Int]): Option[A] = {
+      val index = S.value(selector)
+      if (index >= 0 && index < x.length) Some(x(index)) else None
+    }
   }
 
-  given [A, B] as Select2[Map[A, B], A] {
+  given [A, B] as Select2[Map[A, B], A, A] {
     type Out = B
-    def (x: Map[A, B]) select (selector: A) given (S: Selector[A]): Option[B] =
+    def (x: Map[A, B]) select (selector: A) given (S: Selector[A, A]): Option[B] =
       x.get(S.value(selector))
   }
 }
