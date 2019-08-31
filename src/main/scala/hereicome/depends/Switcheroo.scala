4710e9bd31d0ff2297d867b8ce3743512c7535ce
Walter Chang <weihsiu@gmail.com>
2019-09-01 02:47:55 +0800

    another dependent type example

---
+++ src/main/scala/hereicome/depends/Switcheroo.scala
@@ -0,0 +1,27 @@
+package hereicome.depends
+
+import hereicome._
+
+trait Switcheroo[A] {
+  type Type
+  def apply(x: A): Type
+}
+
+given as Switcheroo[Boolean] {
+  type Type = Int
+  def apply(x: Boolean) = if x then 1 else 0
+}
+
+given as Switcheroo[Int] {
+  type Type = Boolean
+  def apply(x: Int) = x != 1
+}
+
+def switcheroo[A](x: A) given (S: Switcheroo[A]): S.Type = S(x)
+
+val r1: Boolean = switcheroo(0)
+val r2: Int = switcheroo(r1)
+
+// def switcherloop[A](n: Int, x: A) given(S: Switcheroo[A]): S.Type = {
+//   if n == 0 then x else if n % 2 == 0 then switcherloop(n - 1, S(x))
+// }
