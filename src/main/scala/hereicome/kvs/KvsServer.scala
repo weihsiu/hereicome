88b04275a5f4916c9d359da7ec846e65c10eadfd
Walter Chang <weihsiu@gmail.com>
2019-09-04 02:21:47 +0800

    kvs server in progress

--- src/main/scala/hereicome/kvs/KvsServer.scala
+++ src/main/scala/hereicome/kvs/KvsServer.scala
@@ -1,14 +1,24 @@
 package hereicome.kvs
 
+import cats.effect._
+import cats.implicits._
 import java.net.{ServerSocket, Socket}
+import java.util.concurrent.Executors
+import scala.concurrent.ExecutionContext
 
 object KvsServer extends App {
+  def process(socket: Socket) given ContextShift[IO]: IO[Unit] = {
+    import given NetIO._
+    for {
+      b <- socket.readByte
+      _ <- socket.writeByte(b)
+    } yield ()
+  }
   val serverSocket = ServerSocket(3000)
+  given as ContextShift[IO] = IO.contextShift(ExecutionContext.fromExecutorService(Executors.newCachedThreadPool))
   while (true) {
     val socket = serverSocket.accept
-    process(socket)
-  }
-  def process(socket: Socket): Unit = {
-    
+    process(socket).unsafeRunSync
+    socket.close
   }
 }
--- src/main/scala/hereicome/kvs/NetIO.scala
+++ src/main/scala/hereicome/kvs/NetIO.scala
@@ -1,7 +1,8 @@
 package hereicome.kvs
 
-import cats.effect.IO
+import cats.effect._
 import cats.implicits._
+import java.io.{DataInputStream, DataOutputStream}
 import java.net.Socket
 
 trait NetIO[F[_]] {
@@ -12,10 +13,21 @@ trait NetIO[F[_]] {
 }
 
 object NetIO {
-  given as NetIO[IO] {
-    def (socket: Socket) readByte = socket.getInputStream.read
-    def (socket: Socket) readBytes: IO[Vector[Byte]]
-    def (socket: Socket) writeByte(byte: Byte): IO[Unit]
-    def (socket: Socket) writeBytes(bytes: Vector[Byte]): IO[Unit]
+  given as NetIO[IO] given ContextShift[IO] {
+    def block[A](thunk: => A): IO[A] = Blocker[IO].use(_.delay(thunk))
+    def (socket: Socket) readByte = block(socket.getInputStream.read.toByte)
+    def (socket: Socket) readBytes = block {
+      val dis = DataInputStream(socket.getInputStream)
+      val bs = Array.ofDim[Byte](dis.readInt)
+      dis.readFully(bs)
+      bs.toVector
+    }
+    def (socket: Socket) writeByte(byte: Byte): IO[Unit] = block(socket.getOutputStream.write(byte))
+    def (socket: Socket) writeBytes(bytes: Vector[Byte]): IO[Unit] = block {
+      val dos = DataOutputStream(socket.getOutputStream)
+      dos.writeInt(bytes.length)
+      val bs = bytes.toArray
+      dos.write(bs, 0, bs.length)
+    }
   }
 }
