5f94400ee0072d80b39876f29db86873f37a4428
Walter Chang <weihsiu@gmail.com>
2019-09-17 02:03:37 +0800

    try implicit function type

--- src/main/scala/hereicome/actors/ActorTest.scala
+++ src/main/scala/hereicome/actors/ActorTest.scala
@@ -5,10 +5,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 @main def testActor(): Unit =
   import scala.concurrent.ExecutionContext.Implicits.global
   val actor = new Actor[Int | String]:
-    def receive(msg: Int | String) = msg match {
+    def receive(msg: Int | String) = msg match
       case n: Int => println(n + 1)
       case s: String => println(s.toUpperCase)
-    }
   actor.send(1)
   actor.send("hello")
   // actor.send(true)
---
+++ src/main/scala/hereicome/table.scala
@@ -0,0 +1,42 @@
+package hereicome
+
+import scala.collection.mutable.ArrayBuffer
+
+class Table:
+  val rows = ArrayBuffer.empty[Row]
+  def add(r: Row): Unit = rows += r
+  override def toString = rows.mkString("Table(", ",", ")")
+
+class Row:
+  val cells = ArrayBuffer.empty[Cell]
+  def add(c: Cell): Unit = cells += c
+  override def toString = cells.mkString("Row(", ", ", ")")
+
+case class Cell(elem: String)
+
+def table(init: given Table => Unit): Table =
+  given t as Table
+  init
+  t
+
+def row(init: given Row => Unit) given (t: Table) =
+  given r as Row
+  init
+  t.add(r)
+
+def cell(str: String) given (r: Row) =
+  r.add(Cell(str))
+
+@main def testTable() =
+  println(
+    table {
+      row {
+        cell("top left")
+        cell("top right")
+      }
+      row {
+        cell("bottom left")
+        cell("bottom right")
+      }
+    }
+  )
