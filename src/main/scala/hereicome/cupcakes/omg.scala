ae4837b65c78330e123a21272199f790055ee34f
Walter Chang <weihsiu@gmail.com>
2019-09-18 11:14:41 +0800

    more cupcake features

--- src/main/scala/hereicome/cupcakes/omg.scala
+++ src/main/scala/hereicome/cupcakes/omg.scala
@@ -1,48 +1,50 @@
 package hereicome.cupcakes
 
-type >:>[A, B] = given A => B
+type |=>[A, B] = given A => B
 
 trait FileService:
-  def write(data: String): Unit
+  def write(data: String): Unit =
+    println(s"writing $data")
 object FileService:
-  def write(data: String): FileService >:> Unit = the[FileService].write(data)
+  def write(data: String): FileService |=> Unit =
+    the[FileService].write(data)
 
 trait DatabaseService:
-  def insert(data: String): Int
+  def insert(data: String): Int =
+    println(s"inserting $data")
+    123
 
 trait NetworkService:
-  def transmit(data: String): Boolean
+  def transmit(data: String): Boolean = 
+    println(s"transmitting $data")
+    true
 
-trait LogService:
-  def log(data: String): FileService >:> Unit
+trait LogService(val prefix: String):
+  def log(data: String): FileService |=> Unit =
+    the[FileService].write(s"log $prefix:$data")
 
 type AllServices = FileService & DatabaseService & NetworkService & LogService
 
-def persist(name: String): (FileService & DatabaseService) >:> Int = given ctx =>
+def persist(name: String): FileService & DatabaseService |=> Int = given ctx =>
   import FileService._
   write(name)
   ctx.insert(name)
 
-def send(name: String): (DatabaseService & NetworkService) >:> Boolean = given ctx =>
+def send(name: String): DatabaseService & NetworkService |=> Boolean = given ctx =>
   ctx.insert(name)
   ctx.transmit(name)
 
-def process(name: String): AllServices >:> Boolean = given ctx =>
+def process(name: String): AllServices |=> Boolean = given ctx =>
+  given mock as FileService, DatabaseService, NetworkService, LogService("mock"):
+    export ctx._
+    override def write(data: String) = println(s"mock writing $data")
   ctx.log(name)
   persist(name)
   send(name)
 
 @main def testOMG() =
-  given universe as FileService, DatabaseService, NetworkService, LogService:
-    def write(data: String) = println(s"writing $data")
-    def insert(data: String) =
-      println(s"inserting $data")
-      123
-    def transmit(data: String) =
-      println(s"transmitting $data")
-      true
-    def log(data: String) = // given fs =>
-      the[FileService].write(s"log $data")
+  class Universe extends FileService with DatabaseService with NetworkService with LogService("omg")
+  given as Universe
 
   persist("adrian")
   send("brian")
